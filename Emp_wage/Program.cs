c10397b110d1272bc32f77881842616aef9d0554
chandu00081 <chandu00081@gmail.com>
2023-02-05 17:21:50 +0530

    added

---
+++ Emp_wage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int value = r.Next(0, 2);
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
