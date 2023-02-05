e5c0c697439eca52983b1fd22fadd8266348a9d4
chandu00081 <chandu00081@gmail.com>
2023-02-05 18:27:46 +0530

    part time

---
+++ part time emp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagePerHr = 20;
+            int wage = 0, empHr = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == 0)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+
+            wage = wagePerHr * empHr;
+            Console.WriteLine("Total Wage=" + wage);
+        }
+    }
+}
